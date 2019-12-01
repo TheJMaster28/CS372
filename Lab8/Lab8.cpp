7cc04f35012abaf449974176318a2b1f2781a4cf
Jeffrey Lansford <lansfordjr14@gmail.com>
2019-11-30 21:44:18 -0700

    work from last week

--- Lab8/Lab8.cpp
+++ Lab8/Lab8.cpp
@@ -1,19 +1,55 @@
 #include "Lab8.h"
 
+double infin() { return numeric_limits<double>::infinity(); }
+
+double distance(Node &a, Graph &G) { return G.NodeGetDistance(a); }
+
+void distance(Node &a, Graph &G, double dis) { G.NodeSetDistance(a, dis); }
+
+list<Node> make_Queue(Graph &G) {
+    list<Node> Pqueue;
+    for (size_t i = 0; i < G.num_nodes(); i++) {
+        Pqueue.push_back(G.getNode(i));
+    }
+}
+
+Node deleteMin(list<Node> H, Graph &G) {
+    list<Node>::iterator minN;
+    double min = infin();
+
+    // list<Node>::iterator k = bList.begin(); k != bList.end(); k++
+    for (list<Node>::iterator a = H.begin(); a != H.end(); a++) {
+        if (min > distance(*a, G)) {
+            min = distance(*a, G);
+            minN = a;
+        }
+    }
+    H.erase(minN);
+    return *minN;
+}
 
 void Dijkstra_list(Graph &G, Node &s) {
+    G.Reset();
 
+    distance(s, G, 0);
+
+    list<Node> H = make_Queue(G);
+
+    while (!H.empty()) {
+        Node u = deleteMin(H, G);
+        for (Node v : G.getAdjNodes(u)) {
+                }
+    }
 }
 
-void Dijkstra_heap(Graph &G, Node &s) {
-    
-}
+void Dijkstra_heap(Graph &G, Node &s) {}
 
 bool testall() {
-    
+    Graph g("test.txt");
+    cout << g;
 }
 
 int main() {
-
+    testall();
     return 0;
 }
--- Lab8/Lab8.h
+++ Lab8/Lab8.h
@@ -1,238 +1,253 @@
-#include <iostream>
-#include <sstream>
 #include <fstream>
-#include <string>
-#include <vector>
-#include <list>
-#include <unordered_map>
-#include <stack>
-#include <queue>
+#include <iostream>
 #include <limits>
+#include <list>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 class Node {
+   private:
+    string m_name;
+    int m_id;
+    int pre;
+    int post;
+    bool visted;
+    double distance;
 
-    private:
-        string m_name;
-        int m_id;
-        int pre;
-        int post;
-        bool visted;
-        double distance;
-    public:
-        Node() { 
-        };
-        Node( const string & name, int id)
-            {m_name = name, m_id = id; pre = 0; post = 0; visted=false; distance = numeric_limits<double>::infinity(); };
-        int id () const { return m_id;};
-        const string & name() const { return m_name;};
-        void setPre(int i) {pre = i;}
-        void setPost( int i ) {post = i;}
-        void setVisted( bool i) {visted = i;}
-        void setDistance ( double i) {distance = i;}
-        int getPre() const {return pre;}
-        int getPost() const {return post;}
-        bool getVisted() const {return visted;}
-        double getDistance() const {return distance;}
+   public:
+    Node(){};
+    Node(const string &name, int id) {
+        m_name = name, m_id = id;
+        pre = 0;
+        post = 0;
+        visted = false;
+        distance = numeric_limits<double>::infinity();
+    };
+    int id() const { return m_id; };
+    const string &name() const { return m_name; };
+    void setPre(int i) { pre = i; }
+    void setPost(int i) { post = i; }
+    void setVisted(bool i) { visted = i; }
+    void setDistance(double i) { distance = i; }
+    int getPre() const { return pre; }
+    int getPost() const { return post; }
+    bool getVisted() const { return visted; }
+    double getDistance() const { return distance; }
 };
 
 class Graph {
-    private:
-        vector< Node > m_nodes;
-        vector< list<Node> > m_adjList;
-        string fileName;
+   private:
+    vector<Node> m_nodes;
+    vector<list<Node> > m_adjList;
+    string fileName;
 
+   public:
+    unordered_map<string, double> weightList;
 
-    public:
-        
-        Graph ( const string & file )
-            { 
-                fileName = file;
-                scan(file);
-                string NewFileName = file.substr(0,(file.size()-4));
-                save(NewFileName.append("_output.txt"));
-                // cout<<"Done with: "<<NewFileName<<endl;
-            };
+    Graph(const string &file) {
+        fileName = file;
+        scan(file);
+        string NewFileName = file.substr(0, (file.size() - 4));
+        save(NewFileName.append("_output.txt"));
+        // cout<<"Done with: "<<NewFileName<<endl;
+    };
 
-        void addEdge ( const Node & a, const Node & b, double weight) {
-            list<Node> l = m_adjList[a.id()];
-            l.push_back(b);
-            m_adjList[a.id()] = l;
-            
+    void addEdge(const Node &a, const Node &b, double weight) {
+        list<Node> l = m_adjList[a.id()];
+        l.push_back(b);
+        m_adjList[a.id()] = l;
+        string ab = a.name() + "-" + b.name();
+        pair<string, double> abWeight(ab, weight);
+        weightList.insert(abWeight);
+    }
+
+    string getFileName() { return fileName; }
+
+    void addNode(const Node &a) { m_nodes[a.id()] = a; };
+
+    const Node &getNode(size_t i) const { return m_nodes[i]; }
+
+    void clearAdjNodes(const Node &a) {
+        list<Node> empty;
+        m_adjList[a.id()] = empty;
+    }
+
+    list<Node> &getAdjNodes(const Node &a) { return m_adjList[a.id()]; }
+
+    const list<Node> &getAdjNodes(const Node &a) const {
+        return m_adjList[a.id()];
+    }
+
+    size_t num_nodes() const { return m_nodes.size(); }
+
+    string NodeGetName(Node &a) { return getNode(a.id()).name(); }
+
+    int NodeGetPre(Node &a) { return getNode(a.id()).getPre(); }
+
+    int NodeGetPost(Node &a) { return getNode(a.id()).getPost(); }
+
+    bool NodeGetVist(Node &a) { return getNode(a.id()).getVisted(); }
+
+    double NodeGetDistance(Node &a) { return getNode(a.id()).getDistance(); }
+
+    void NodeSetPre(Node &a, int i) {
+        Node node = getNode(a.id());
+        node.setPre(i);
+        addNode(node);
+    }
+
+    void NodeSetPost(Node &a, int i) {
+        Node node = getNode(a.id());
+        node.setPost(i);
+        addNode(node);
+    }
+
+    void NodeSetVisted(Node &a, bool i) {
+        Node node = getNode(a.id());
+        node.setVisted(i);
+        addNode(node);
+    }
+
+    void NodeSetDistance(Node &a, double i) {
+        Node node = getNode(a.id());
+        node.setDistance(i);
+        addNode(node);
+    }
+
+    double GetWeight(Node &a, Node &b) const {
+        string ab = a.name() + "-" + b.name();
+        unordered_map<string, double>::const_iterator weight =
+            weightList.find(ab);
+        return weight->second;
+    }
+
+    void Reset() {
+        for (size_t i = 0; i < m_nodes.size(); i++) {
+            NodeSetVisted(m_nodes[i], false);
+            NodeSetPre(m_nodes[i], 0);
+            NodeSetPost(m_nodes[i], 0);
+            NodeSetDistance(m_nodes[i], numeric_limits<double>::infinity());
         }
-        
-        string getFileName() { return fileName; }
+        cout << "Resetting Graph...\n" << endl;
+    }
 
-        void addNode( const Node & a ) { m_nodes[a.id()] = a; };
+    void scan(const string &file) {
+        ifstream myFile;
+        myFile.open(file);
+        string c1, c2;
+        double c3;
+        size_t id = 0;
 
-        const Node & getNode(size_t i) const { return m_nodes[i]; }
+        // using hash table
+        unordered_map<string, size_t> nodeMap;
+        // unordered_map< string, string > edgeMap;
 
-        void clearAdjNodes ( const Node & a) {
-            list<Node> empty;
-            m_adjList[a.id()] = empty;
-        }
+        if (myFile.is_open()) {
+            // goes through file and adds nodes to a hash table
+            while (myFile >> c1 >> c2 >> c3) {
+                // Find node to make sure it is not already found
+                unordered_map<string, size_t>::const_iterator nodeA =
+                    nodeMap.find(c1);
 
-        list<Node> & getAdjNodes ( const Node & a)
-            {return m_adjList[a.id()];}
-        
-        const list<Node> & getAdjNodes ( const Node & a) const
-            {return m_adjList[a.id()];}
-        
-        size_t num_nodes() const { return m_nodes.size(); }
-
-        string NodeGetName( Node &a) { return getNode(a.id()).name(); }
-
-        int NodeGetPre( Node & a) { return getNode(a.id()).getPre(); }
-
-        int NodeGetPost( Node & a) { return getNode(a.id()).getPost(); }
-        
-        bool NodeGetVist( Node & a) { return getNode(a.id()).getVisted(); }
-
-        double NodeGetDistance( Node &a) {return getNode(a.id()).getDistance(); }
-
-        void NodeSetPre ( Node & a, int i) { 
-            Node node = getNode(a.id()); 
-            node.setPre(i);
-            addNode(node);
-        }
-
-        void NodeSetPost ( Node & a, int i) { 
-            Node node = getNode(a.id()); 
-            node.setPost(i);
-            addNode(node);
-        }
-
-        void NodeSetVisted( Node & a, bool i) { 
-            Node node = getNode(a.id());
-            node.setVisted(i);
-            addNode(node);
-        }
-
-        void NodeSetDistance( Node &a, double i) {
-            Node node = getNode(a.id());
-            node.setDistance(i);
-            addNode(node);
-        }
-
-        void Reset() {
-            for ( size_t i = 0; i < m_nodes.size(); i++ ) {
-                NodeSetVisted(m_nodes[i],false);
-                NodeSetPre(m_nodes[i], 0);
-                NodeSetPost(m_nodes[i], 0);
-                NodeSetDistance(m_nodes[i], numeric_limits<double>::infinity());
-            }
-		    cout<<"Resetting Graph...\n"<<endl;
-        }
-
-        
-
-        void scan(const string & file ){
-            ifstream myFile;
-            myFile.open( file );
-            string c1, c2;
-            double c3;
-            size_t id = 0;
-
-            // using hash table
-            unordered_map<string, size_t > nodeMap;
-            // unordered_map< string, string > edgeMap; 
-
-            if ( myFile.is_open()) {
-                
-                // goes through file and adds nodes to a hash table  
-                while ( myFile >> c1 >> c2) {
-                    // Find node to make sure it is not already found  
-                    unordered_map< string, size_t >::const_iterator nodeA = nodeMap.find(c1);
-                    
-                    // if nout found add to hash table
-                    if ( nodeA == nodeMap.end() ) {
-                        pair< string, size_t > node1 ( c1, id++ );
-                        nodeMap.insert(node1);
-
-                    }
-                    
-                    unordered_map< string, size_t >::const_iterator nodeB = nodeMap.find(c2);
-
-                    if ( nodeB == nodeMap.end() ) {
-                        pair< string, size_t > node2 ( c2, id++ );
-                        nodeMap.insert(node2);
-                    }
-                    
+                // if nout found add to hash table
+                if (nodeA == nodeMap.end()) {
+                    pair<string, size_t> node1(c1, id++);
+                    nodeMap.insert(node1);
                 }
 
-                // resizes Vectors to make inserting elements quicker and have allcated space
-                m_nodes.resize(nodeMap.size());
-                m_adjList.resize(nodeMap.size());
+                unordered_map<string, size_t>::const_iterator nodeB =
+                    nodeMap.find(c2);
 
-                // goes through hash table of nodes and adds them to m_nodes vector
-                for ( pair <string, size_t> node : nodeMap ) {
-                    Node a ( node.first, node.second );
-                    addNode(a);
-                }
-
-                // reopen file
-                myFile.close();
-                myFile.open(file);
-                
-                // goes through file again and adds edges to the m_adjList vector
-                while ( myFile >> c1 >> c2 >> c3) {
-                    
-                    // find both nodes in hash table
-                    unordered_map< string, size_t >::const_iterator nodeA = nodeMap.find(c1);
-                    unordered_map< string, size_t >::const_iterator nodeB = nodeMap.find(c2);
-                    
-                    // get both nodes and add edge
-                    Node a = getNode(nodeA->second);
-                    Node b = getNode(nodeB->second);
-                    addEdge(a,b, c3);
-                }
-                myFile.close();
-            }
-        }
-
-        void save( const string & file ) const {
-            // get file for outputing 
-            ofstream MyFile;
-            MyFile.open(file);
-
-            // goes through each nodes list and writes the edge tab-spaced
-            for ( size_t i = 0; i < m_nodes.size(); i ++ ) {
-                
-                // get adjacent nodes
-                Node a = getNode(i);
-                list<Node> bList = getAdjNodes(a);
-
-                // goes through list and writes it to output file
-                for ( list<Node>::iterator k = bList.begin(); k != bList.end(); k++ ) {
-                    MyFile<<a.name()<<"\t"<<k->name()<<endl;
+                if (nodeB == nodeMap.end()) {
+                    pair<string, size_t> node2(c2, id++);
+                    nodeMap.insert(node2);
                 }
             }
 
-            MyFile.close();
+            // resizes Vectors to make inserting elements quicker and have
+            // allcated space
+            m_nodes.resize(nodeMap.size());
+            m_adjList.resize(nodeMap.size());
+
+            // goes through hash table of nodes and adds them to m_nodes vector
+            for (pair<string, size_t> node : nodeMap) {
+                Node a(node.first, node.second);
+                addNode(a);
+            }
+
+            // reopen file
+            myFile.close();
+            myFile.open(file);
+
+            // goes through file again and adds edges to the m_adjList vector
+            while (myFile >> c1 >> c2 >> c3) {
+                // find both nodes in hash table
+                unordered_map<string, size_t>::const_iterator nodeA =
+                    nodeMap.find(c1);
+                unordered_map<string, size_t>::const_iterator nodeB =
+                    nodeMap.find(c2);
+
+                // get both nodes and add edge
+                Node a = getNode(nodeA->second);
+                Node b = getNode(nodeB->second);
+                addEdge(a, b, c3);
+            }
+            myFile.close();
+        }
+    }
+
+    void save(const string &file) const {
+        // get file for outputing
+        ofstream MyFile;
+        MyFile.open(file);
+
+        // goes through each nodes list and writes the edge tab-spaced
+        for (size_t i = 0; i < m_nodes.size(); i++) {
+            // get adjacent nodes
+            Node a = getNode(i);
+            list<Node> bList = getAdjNodes(a);
+
+            // goes through list and writes it to output file
+            for (list<Node>::iterator k = bList.begin(); k != bList.end();
+                 k++) {
+                unordered_map<string, double>::const_iterator weight =
+                    weightList.find(a.name() + "-" + k->name());
+                MyFile << a.name() << "\t" << k->name() << "\t"
+                       << weight->second << endl;
+            }
         }
 
+        MyFile.close();
+    }
 };
 
-std::ostream& operator<<(std::ostream& out, const Graph & g)
-{
+std::ostream &operator<<(std::ostream &out, const Graph &g) {
     out << "Nodes in the graph:" << endl;
-    for ( unsigned i=0; i <g.num_nodes(); i++ ) {
+    for (unsigned i = 0; i < g.num_nodes(); i++) {
         Node a = g.getNode(i);
-        out << a.name()<< " Pre: "<<a.getPre()<<" Post: "<<a.getPost()<<" Distance: "<<a.getDistance()<<endl;
+        out << a.name() << " Pre: " << a.getPre() << " Post: " << a.getPost()
+            << " Distance: " << a.getDistance() << endl;
     }
     out << endl;
     out << "Adjacency list of the graph :" << endl;
-    for ( unsigned i=0; i<g.num_nodes(); i++) {
-        out << "Node " << g.getNode(i).name() << ", " <<g.getNode(i).id() << ":";
-        const list <Node> neighbors = g.getAdjNodes(g.getNode(i));
-        for ( list <Node>::const_iterator itr = neighbors.begin(); itr != neighbors.end(); ++itr) {
-            out << itr -> name() << ",";
+    for (unsigned i = 0; i < g.num_nodes(); i++) {
+        out << "Node " << g.getNode(i).name() << ", ID:" << g.getNode(i).id()
+            << ": ";
+        const list<Node> neighbors = g.getAdjNodes(g.getNode(i));
+        for (list<Node>::const_iterator itr = neighbors.begin();
+             itr != neighbors.end(); ++itr) {
+            Node a = g.getNode(i);
+            Node b = *itr;
+            double weight = g.GetWeight(a, b);
+            out << itr->name() << ": " << weight << ", ";
         }
         out << endl;
     }
 
     return out;
 }
-
-
-    
